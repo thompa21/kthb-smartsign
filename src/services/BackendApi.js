574c2c71d1e19e4845d40bf42cf732416e2a5033
Thomas Lind <tholind@UG.KTH.SE>
2018-03-20 10:13:39 +0100

    commit

--- src/services/BackendApi.js
+++ src/services/BackendApi.js
@@ -7,18 +7,18 @@ function getEntriesData() {
     startofday.setHours(0,0,0,0);
     var endofday = new Date();
     endofday.setHours(23,59,59,0);
-    const url = `${apiUrl}mrbs/api/v1/noauth/entries/?limit=1000&area_id=2&fromDate=${startofday.getTime()/1000}&toDate=${endofday.getTime()/1000}`;
+    const url = apiUrl + 'mrbs/api/v1/noauth/entries/?limit=1000&area_id=2&fromDate=' + startofday.getTime()/1000 + '&toDate=' + endofday.getTime()/1000;
     return axios.get(url);
 }
 
 function getRoomsData() {
-    const url = `${apiUrl}mrbs/api/v1/noauth/rooms/?limit=1000&area_id=2}`;
+    const url = apiUrl + 'mrbs/api/v1/noauth/rooms/?limit=1000&area_id=2';
     return axios.get(url);
 }
 
 function getRoomAvailability() {
     var currenttimestamp = Math.floor(Date.now() /1000);
-    const url = `${apiUrl}mrbs/api/v1/noauth/roomsavailability/?timestamp=${currenttimestamp}&limit=1000&area_id=2}`;
+    const url = apiUrl + 'mrbs/api/v1/noauth/roomsavailability/?timestamp=' + currenttimestamp + '&limit=1000&area_id=2';
     return axios.get(url);
 }
 
